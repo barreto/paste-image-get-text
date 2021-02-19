8dd5d146b68f1c568b34d179793def4ab868a70c
Barreto <v.barreto.and@gmail.com>
2021-02-18 23:37:01 -0300

    feat: using ocr with pasted image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
 
-// Caso esteja compartilhando tela não estamos vendo, professor
+type info = { imgSrc: string; text: string };
 
 function App() {
-  const [images, setImages] = useState<string[]>([]);
+  const [infos, setInfos] = useState<info[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [worker, setWorker] = useState(createWorker());
 
   const handleOnPaste = async (event: any) => {
     event.preventDefault();
@@ -14,35 +17,96 @@ function App() {
     let file = event.clipboardData.items[0].getAsFile();
     let objectUrl = URL.createObjectURL(file);
 
-    setImages([...images, objectUrl]);
+    setInfos([...infos, { imgSrc: objectUrl, text: "Loading..." }]);
+
+    doOCR(objectUrl);
   };
 
-  return (
-    <div onPaste={handleOnPaste}>
-      <h1>Paste image and get text</h1>
+  const initWorker = async () => {
+    setIsLoading(true);
+    const initialWorker = createWorker();
+    await initialWorker.load();
+    await initialWorker.loadLanguage("por+eng");
+    await initialWorker.initialize("por+eng");
+    setWorker(initialWorker);
+    setIsLoading(false);
+  };
 
-      {images.map((imgSrc: string, idx: number) => {
-        // return <img src={imgSrc} />;
-        return (
-          <div
-            key={idx}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-around",
-              padding: "8px",
-            }}
-          >
-            <div className="images-container">
-              <img src={imgSrc} />
-            </div>
-            <div className="texts-container">
-              <textarea cols={30} rows={10} value="test next line"></textarea>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+  const doOCR = async (src: string) => {
+    const {
+      data: { text },
+    } = await worker.recognize(src);
+    await worker.terminate();
+
+    // const {
+    //   data: { text },
+    // } = { data: { text: "This is a very nice test! :D" } };
+
+    const newInfos = [...infos, { imgSrc: src, text: "" }];
+    setInfos(newInfos);
+
+    const callInsertText = () => insertText(src, text, newInfos);
+    const timeout = 500;
+    setTimeout(callInsertText, timeout);
+  };
+
+  const insertText = (src: string, text: string, oldInfos: info[]) => {
+    const newInfos = [...oldInfos];
+    const indexOfSrc = newInfos.findIndex((info) => info.imgSrc === src);
+    newInfos[indexOfSrc].text = text;
+    setInfos(newInfos);
+  };
+
+  //=========================
+  useEffect(() => {
+    initWorker();
+  }, []);
+
+  function getLoading() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Loading ...
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {isLoading ? (
+        getLoading()
+      ) : (
+        <div onPaste={handleOnPaste}>
+          <h1>Paste image and get text</h1>
+
+          {infos.map((info: info, idx: number) => {
+            return (
+              <div
+                key={idx}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                  padding: "8px",
+                }}
+              >
+                <div className="images-container">
+                  <img src={info.imgSrc} alt="Pasted information" />
+                </div>
+                <div className="texts-container">
+                  <p>{info.text}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
 
